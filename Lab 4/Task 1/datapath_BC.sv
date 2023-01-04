e5dba9005314ad66c8bd1c9db23e5d41ef2bdd25
garrettvt <77711492+garrettvt@users.noreply.github.com>
2023-01-03 18:48:20 -0800

    Fixing indentations

--- Lab 4/Task 1/datapath_BC.sv
+++ Lab 4/Task 1/datapath_BC.sv
@@ -11,48 +11,48 @@
 //bit counter to count the number of 1's in a certain set of data
 //being passed in. 
 module datapath_BC #(parameter width = 8)(clk, reset, A, load_b, result_shift, done, z, result);
-		input logic 			      clk, reset;
-		input logic 			      load_b, result_shift, done;
-		input logic [width - 1: 0] A;
-		output logic 			      z;
-		output logic [3:0]		   result; 
-		
-		//logic to hold value of A that is orignially
-		//passed in so the data can be shifted to 
-		//count the number of 1's in the data passed.
-		//4-bit logic count to hold the count of number
-		//of 1's that the data has. 
-		logic [width - 1: 0] new_a;
-		logic [3:0] count;
-		
-		//Assign output z to not or new_a
-		assign z = ~|new_a; 
-		
-		//Assign result to be 0 if done is 0,
-		//or for result to equal the count if
-		//done is 1 using a conditional operator.
-		assign result = (done) ? count : 4'd0;
-		
-		//This always_ff block has the logic for the 
-		//datapath. If reset or load_b are 1, then 
-		//count is set to 0 and new_a is set to the 
-		//input data. Else if right_shift is 1, if
-		//the data in the 0'th place of a is a 1 then
-		//increase count by 1, and always shift new_a
-		//to the right by 1.
-		always_ff @(posedge clk) begin
-			if(reset || load_b) begin
-				count <= 0;
-				new_a <= A;
-			end
-			
-			else if(result_shift) begin
-				if(new_a[0] == 1) begin
-					count <= count + 4'd1;
-				end
-				new_a <= new_a >> 1;
-			end
+	input logic 		   clk, reset;
+	input logic 		   load_b, result_shift, done;
+	input logic [width - 1: 0] A;
+	output logic 		   z;
+	output logic [3:0]	   result; 
+
+	//logic to hold value of A that is orignially
+	//passed in so the data can be shifted to 
+	//count the number of 1's in the data passed.
+	//4-bit logic count to hold the count of number
+	//of 1's that the data has. 
+	logic [width - 1: 0] new_a;
+	logic [3:0] count;
+
+	//Assign output z to not or new_a
+	assign z = ~|new_a; 
+
+	//Assign result to be 0 if done is 0,
+	//or for result to equal the count if
+	//done is 1 using a conditional operator.
+	assign result = (done) ? count : 4'd0;
+
+	//This always_ff block has the logic for the 
+	//datapath. If reset or load_b are 1, then 
+	//count is set to 0 and new_a is set to the 
+	//input data. Else if right_shift is 1, if
+	//the data in the 0'th place of a is a 1 then
+	//increase count by 1, and always shift new_a
+	//to the right by 1.
+	always_ff @(posedge clk) begin
+		if(reset || load_b) begin
+			count <= 0;
+			new_a <= A;
 		end
+
+		else if(result_shift) begin
+			if(new_a[0] == 1) begin
+				count <= count + 4'd1;
+			end
+			new_a <= new_a >> 1;
+		end
+	end
 endmodule 
 
 //datapath_BC_testbench tests for expected, unexpected, and edgecase
@@ -67,34 +67,34 @@ endmodule
 //values being passed to the system, and that the output values were getting
 //updated correctly after the input values were cahnged. 
 module datapath_BC_testbench();
-		logic clk, reset, load_b, result_shift, done, z;
-		logic [7:0] A;
-		logic [3:0] result;
-		
-		datapath_BC #(8) dut(.clk, .reset, .A, .load_b, .result_shift, .done, .z, .result);
-		
-		parameter clk_PERIOD = 100;            
-		initial begin                
-				clk <= 0;
-				forever #(clk_PERIOD/2) clk <= ~clk;// Forever toggle the clk     
-		end
-		
-		initial begin
-			A <= 8'b01100111;	load_b <= 0; result_shift <= 0; done <= 0; repeat(1)   @(posedge    clk);
-			reset <= 1; 			repeat(1)   @(posedge    clk);
-			reset <= 0; 			repeat(1)   @(posedge    clk);
-			result_shift <= 1;	repeat(8)   @(posedge    clk);
-			result_shift <= 0;	repeat(1)   @(posedge    clk);
-			done <= 1;				repeat(1)   @(posedge    clk);
-			done <= 0;				repeat(1)   @(posedge    clk);
-			A <= 8'b11000011;		repeat(1)   @(posedge    clk);
-			load_b <= 1;			repeat(1)   @(posedge    clk);
-			load_b <= 0;			repeat(1)   @(posedge    clk);
-			result_shift <= 1;	repeat(8)   @(posedge    clk);
-			result_shift <= 0;	repeat(1)   @(posedge    clk);
-			done <= 1;				repeat(1)   @(posedge    clk);
-			done <= 0;				repeat(1)   @(posedge    clk);
-			
-			$stop; // End the simulation.
-		end
+	logic clk, reset, load_b, result_shift, done, z;
+	logic [7:0] A;
+	logic [3:0] result;
+
+	datapath_BC #(8) dut(.clk, .reset, .A, .load_b, .result_shift, .done, .z, .result);
+
+	parameter clk_PERIOD = 100;            
+	initial begin                
+	      clk <= 0;
+	      forever #(clk_PERIOD/2) clk <= ~clk;// Forever toggle the clk     
+	end
+
+	initial begin
+	   A <= 8'b01100111;	load_b <= 0; result_shift <= 0; done <= 0; repeat(1)   @(posedge    clk);
+	   reset <= 1; 		repeat(1)   @(posedge    clk);
+	   reset <= 0; 		repeat(1)   @(posedge    clk);
+	   result_shift <= 1;	repeat(8)   @(posedge    clk);
+	   result_shift <= 0;	repeat(1)   @(posedge    clk);
+	   done <= 1;		repeat(1)   @(posedge    clk);
+	   done <= 0;		repeat(1)   @(posedge    clk);
+	   A <= 8'b11000011;	repeat(1)   @(posedge    clk);
+	   load_b <= 1;		repeat(1)   @(posedge    clk);
+	   load_b <= 0;		repeat(1)   @(posedge    clk);
+	   result_shift <= 1;	repeat(8)   @(posedge    clk);
+	   result_shift <= 0;	repeat(1)   @(posedge    clk);
+	   done <= 1;		repeat(1)   @(posedge    clk);
+	   done <= 0;		repeat(1)   @(posedge    clk);
+
+	   $stop; // End the simulation.
+	end
 endmodule 
